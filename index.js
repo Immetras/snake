230d17490065b60982d5a159e72b29f145158ed8
immetras <eranusieq@gmail.com>
2024-01-12 16:39:32 +0100

    just the uhh, graphic

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ function main() {
 
   //* generate
   const size = 15;
-  const board = document.getElementById("boardTable");
+  const board = document.getElementById("board");
 
   for (let i = 0; i < size + 2; i++) {
     const tr = document.createElement("tr");
 
-    for (let j = 0; j < size; j++) {
+    for (let j = 0; j < size + 2; j++) {
       const td = document.createElement("td");
 
-      switch ((i, j)) {
-        case 0:
-        case size + 1:
+      switch (true) {
+        case i == 0:
+        case j == 0:
+        case i == size + 1:
+        case j == size + 1:
           td.className = "border";
-
           break;
-
         default:
           break;
       }
